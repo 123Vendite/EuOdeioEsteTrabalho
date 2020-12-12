4d19510b0f2a3553b71e383cfb5f660bfe20eb37
123Vendite <lucasmacedovendite@hotmai.com>
2020-12-11 20:33:52 -0400

    Correção SW

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(evt) =>){
+  console.log("Install SW")
+}
+
+self.addEventListener('activate', function(evt) =>){
+  console.log("Activate SW")
+}
+
+self.addEventListener('fetch', function(evt) =>){
+  console.log("fetch SW")
+}
